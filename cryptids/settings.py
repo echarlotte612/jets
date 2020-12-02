e54455e3a906df09a7c23f89330654aaee0b6896
ecbaldocke612 <e.charlotte94@gmail.com>
2020-12-02 11:04:18 +0000

    added os to settings file

--- cryptids/settings.py
+++ cryptids/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import connfig
 from dj_database_url import parse as db_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
